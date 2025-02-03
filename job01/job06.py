8829f69dfe9f6f5b52c87508d33d6ed6c4ab2ee3
morgane-rossi <morgane_rt@hotmail.com>
2025-02-03 18:16:55 +0100

    Jour 01 : job06

---
+++ job01/job06.py
@@ -0,0 +1,26 @@
+class Animal :
+
+    def __init__(self):
+        """
+        Constructeur qui initialise âge à zéro
+        et prénom à une chaîne de caractères vide
+        """
+        self.age = 0
+        self.prenom = ""
+
+    def afficherAge(self):
+        print(f"L'age de l'animal {self.age} ans")
+
+    def vieillir(self):
+        self.age += 1
+
+    def nommer(self, prenom):
+        self.prenom = prenom
+        print(f"L'animal se nomme {self.prenom}")
+
+# Instanciez un objet Animal et affichez en console l’attribut âge
+mon_chien = Animal()
+mon_chien.afficherAge()
+mon_chien.vieillir()
+mon_chien.afficherAge()
+mon_chien.nommer("Poki")

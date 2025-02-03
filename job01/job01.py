86b77b76951fe2874e998355fb6e3103efc0f940
morgane-rossi <morgane_rt@hotmail.com>
2025-02-03 18:04:14 +0100

    Jour 01 : job01

---
+++ job01/job01.py
@@ -0,0 +1,11 @@
+class Operation :
+
+    def __init__(self):
+        """
+        Constructeur avec des valeurs par défaut
+        """
+        self.nombre1 = 3
+        self.nombre2 = 42
+
+op = Operation()
+print(op)
